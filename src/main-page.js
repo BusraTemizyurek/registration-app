43ad23b6c65977e989392c0cf5d8d9762c0accf7
Busra Temizyurek <busratemizyurek@Busras-MacBook-Pro.local>
2024-10-07 13:57:52 -0700

    first commit

---
+++ src/main-page.js
@@ -0,0 +1,104 @@
+const url = "https://cusco-api.vercel.app";
+
+async function getRecords() {
+    const response = await fetch(url);
+    const records = await response.json();
+
+    return records;
+}
+const records = getRecords();
+
+function createRow(record) {
+    const row = document.createElement("tr");
+
+    const name = document.createElement("td");
+    name.innerText = record.customer;
+
+    const date = document.createElement("td");
+    date.innerText = formatDate(new Date(record.date));
+    date.classList.add("date-column");
+
+    const subject = document.createElement("td");
+    subject.innerText = record.subject;
+
+    const price = document.createElement("td");
+    price.innerText = `$${record.price}`;
+
+    const actions = document.createElement("td");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash" style="color: #353536;"></i>';
+    deleteButton.classList.add("delete-button");
+    deleteButton.onclick = createOnClick(record.id, row, deleteButton);
+
+    const editLink = document.createElement("a");
+    editLink.innerHTML = '<i class="fa-solid fa-pen" style="color: #4c4c4d;"></i>';
+    editLink.href = `new-edit.html?id=${record.id}`
+
+    actions.append(editLink, deleteButton);
+
+    row.append(name, date, subject, price, actions);
+
+    return row;
+}
+
+function createOnClick(id, row, button) {
+    return async function () {
+        if (!confirm("Are you sure that you want to delete the item?")) {
+            return;
+        }
+
+        button.disabled = true;
+        const response = await fetch(`${url}/${id}`, {
+            method: "DELETE"
+        })
+
+        if (response.ok) {
+            const list = document.getElementById("list");
+            list.removeChild(row);
+        } else {
+            button.disabled = false;
+            alert("Error");
+        }
+    }
+}
+
+async function printList(search) {
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+
+    for (const record of (await records)) {
+        if (!search || search.length === 0 || hasSearchInRecord(record, search)) {
+            list.append(createRow(record));
+        }
+    }
+}
+
+//debounce 
+let searchTimeOutId;
+function onSearchKeyUp(event) {
+    if (searchTimeOutId) {
+        clearTimeout(searchTimeOutId);
+        searchTimeOutId = undefined;
+    }
+
+    searchTimeOutId = setTimeout(async () => {
+        const search = event.target.value.toLowerCase();
+        console.log('searching', search);
+        printList(search);
+    }, 400);
+}
+
+function hasSearchInRecord(record, search) {
+    for (const item of Object.values(record)) {
+        if (item.toString().toLowerCase().includes(search)) {
+            return true;
+        }
+    }
+}
+
+window.onpageshow = async function () {
+    const search = document.getElementById("search-bar").value;
+    console.log("search.val:", search);
+    printList(search.toLowerCase());
+}
---
+++ src/new-edit.js
@@ -0,0 +1,110 @@
+const url = "https://cusco-api.vercel.app";
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+console.log(id);
+
+
+function setDateToToday() {
+    const date = document.getElementById("date");
+    const today = new Date();
+    date.value = formatDate(today);
+}
+
+function onInputKeyUp(event) {
+    const input = event.target;
+    if (!input.checkValidity()) {
+        input.classList.add('highlight');
+    } else {
+        input.classList.remove('highlight');
+    }
+}
+
+async function getRecords() {
+    const response = await fetch(url);
+    const records = await response.json();
+
+    return records;
+}
+
+const allRecords = getRecords();
+
+async function onClickSave() {
+    const inputs = [
+        ...document.getElementsByTagName("input"),
+        ...document.getElementsByTagName("textarea")
+    ]
+
+    for (const input of inputs) {
+        if (!input.checkValidity()) {
+            input.classList.add('highlight');
+        }
+    }
+
+    const highlightedInputs = document.getElementsByClassName("highlight");
+    if (highlightedInputs.length > 0) {
+        highlightedInputs[0].focus();
+        return;
+    }
+
+    const user = document.getElementById("uname").value;
+    const branch = document.getElementById("branch").value;
+    const customer = document.getElementById("cname").value;
+    const phone_number = document.getElementById("cphone").value;
+    const subject = document.getElementById("subject").value;
+    const price = document.getElementById("price").value;
+    const explanation = document.getElementById("explanation").value;
+
+    const record = {
+        customer,
+        user,
+        date: (new Date()).toISOString(),
+        branch,
+        subject,
+        price,
+        explanation,
+        phone_number
+    }
+
+    const response = await fetch(id ? `${url}/${id}` : url, {
+        method: id ? "PUT" : "POST",
+        body: JSON.stringify(record),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    if (response.ok) {
+        // redirect to main page
+        location.href = "./main-page.html"
+    } else {
+        alert("Error");
+    }
+
+}
+
+window.onload = async function () {
+
+    if (id) {
+        response = await fetch(`${url}/${id}`, {
+            method: "GET"
+        })
+
+        if (response.ok) {
+            const record = await response.json();
+            console.log(record)
+            document.getElementById("uname").value = record.user;
+            document.getElementById("branch").value = record.branch;
+            document.getElementById("cname").value = record.customer;
+            document.getElementById("cphone").value = record.phone_number;
+            document.getElementById("subject").value = record.subject;
+            document.getElementById("price").value = record.price
+            document.getElementById("explanation").value = record.explanation;
+        } else {
+            alert("Data can not be updated, try again");
+            location.href = "./main-page.html";
+        }
+    }
+
+    setDateToToday();
+}
